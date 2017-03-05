4691d928d89eae531afb066ad3f375e7b0b5c086
babarqb <babarqb@SONY-VAIO>
2017-03-05 11:52:46 +0500

    Add project files.

---
+++ MemobileDialer/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿using System;
+
+namespace MemobileDialer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PortSE = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PHONN = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.R = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(82, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MEMOBILE DIALER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Develop by: babarqb@gmail.com";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "PORTS:";
+            // 
+            // PortSE
+            // 
+            this.PortSE.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PortSE.FormattingEnabled = true;
+            this.PortSE.Items.AddRange(new object[] {
+            "Select"});
+            this.PortSE.Location = new System.Drawing.Point(75, 88);
+            this.PortSE.Name = "PortSE";
+            this.PortSE.Size = new System.Drawing.Size(121, 21);
+            this.PortSE.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phone#:";
+            // 
+            // PHONN
+            // 
+            this.PHONN.Location = new System.Drawing.Point(75, 131);
+            this.PHONN.Name = "PHONN";
+            this.PHONN.Size = new System.Drawing.Size(121, 20);
+            this.PHONN.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(94, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 30);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(148, 177);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 30);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(202, 177);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(48, 30);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(94, 213);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(48, 30);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "4";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(148, 213);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(48, 30);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "5";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(202, 213);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(48, 30);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "6";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(94, 249);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(48, 30);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "7";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(148, 249);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(48, 30);
+            this.button10.TabIndex = 8;
+            this.button10.Text = "8";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(202, 249);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(48, 30);
+            this.button11.TabIndex = 8;
+            this.button11.Text = "9";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(148, 285);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(48, 30);
+            this.button13.TabIndex = 8;
+            this.button13.Text = "0";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(202, 285);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(48, 30);
+            this.button14.TabIndex = 8;
+            this.button14.Text = "Clear";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(69, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "MeMobiles && Computers.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(86, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "+923465395285";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(86, 363);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "+923078928288";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 138);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Device Manger";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackgroundImage = global::MemobileDialer.Properties.Resources._1488676238_circle_back_arrow_glyph;
+            this.button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button12.FlatAppearance.BorderSize = 0;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button12.Location = new System.Drawing.Point(94, 285);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(48, 30);
+            this.button12.TabIndex = 8;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackgroundImage = global::MemobileDialer.Properties.Resources.preview;
+            this.btnSend.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSend.FlatAppearance.BorderSize = 0;
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSend.Location = new System.Drawing.Point(202, 125);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(48, 30);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // R
+            // 
+            this.R.BackgroundImage = global::MemobileDialer.Properties.Resources._1488675891_interact;
+            this.R.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.R.FlatAppearance.BorderSize = 0;
+            this.R.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.R.Location = new System.Drawing.Point(202, 82);
+            this.R.Name = "R";
+            this.R.Size = new System.Drawing.Size(48, 30);
+            this.R.TabIndex = 6;
+            this.R.UseVisualStyleBackColor = true;
+            this.R.Click += new System.EventHandler(this.R_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MemobileDialer.Properties.Resources.preview;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(82, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(269, 388);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.PHONN);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.R);
+            this.Controls.Add(this.PortSE);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Memobile Dialer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void PortSE_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox PortSE;
+        private System.Windows.Forms.Button R;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox PHONN;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MemobileDialer/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MemobileDialer
+{
+    public partial class Form1 : Form
+    {
+
+        private StringBuilder sb;
+        private SerialPort sp = new SerialPort();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            sb = new StringBuilder();
+            foreach (object portName in SerialPort.GetPortNames())
+                this.PortSE.Items.Add(portName);
+            PortSE.SelectedIndex = -1;
+            PortSE.Text = "Select";
+        }
+
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            this.sp.PortName = this.PortSE.Text;
+            this.sp.Close();
+            Thread.Sleep(1000);
+            if (this.PortSE.Text == "Select")
+            {
+                int num1 = (int)MessageBox.Show("Please Select Port !", "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                this.sp.Open();
+                Thread.Sleep(1000);
+                this.sp.Write("at+creg?\r\n");
+                Thread.Sleep(500);
+                this.sp.Write("atd" + this.PHONN.Text + ";\r\n");
+                Thread.Sleep(1000);
+                int num2 = (int)MessageBox.Show("Success !", "Message", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                this.sp.Close();
+            }
+        }
+
+        private void R_Click(object sender, EventArgs e)
+        {
+            if (SerialPort.GetPortNames().Length < 1)
+            {
+                PortSE.Text = "Select";
+                return;
+            }
+
+            this.PortSE.DataSource = (object)SerialPort.GetPortNames();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PortSE.SelectedItem = "Select";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PHONN.Text = sb.Append("1").ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            PHONN.Text = sb.Append("2").ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            PHONN.Text = sb.Append("3").ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            PHONN.Text = sb.Append("4").ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            PHONN.Text = sb.Append("5").ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            PHONN.Text = sb.Append("6").ToString();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            PHONN.Text = sb.Append("7").ToString();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            PHONN.Text = sb.Append("8").ToString();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            PHONN.Text = sb.Append("9").ToString();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            PHONN.Text = sb.Append("0").ToString();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (PHONN.Text == "")
+            {
+                return;
+            }
+            PHONN.Text = sb.Remove(sb.Length - 1, 1).ToString();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+            PHONN.Text = "";
+            sb.Remove(0, sb.Length);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("devmgmt.msc");
+        }
+    }
+}
